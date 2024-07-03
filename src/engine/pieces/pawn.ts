107cd7f9677e3d98a561af6455d47a8c040d8265
Bogdan Rusu <Bogdan.Rusu@softwire.com>
2024-07-03 13:01:24 +0300

    do pawn tests

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,15 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        let newSquare: Square;
+        if (this.player == Player.WHITE)
+            newSquare = new Square(currentSquare.row + 1, currentSquare.col);
+        else
+            newSquare = new Square(currentSquare.row - 1, currentSquare.col);
+        const moveArray: any[] = new Array(0);
+        moveArray.push(newSquare);
+
+        return moveArray;
     }
 }
