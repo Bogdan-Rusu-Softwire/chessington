ecc57ac1a3f00296a5d437d370436d284da87edf
Bogdan Rusu <Bogdan.Rusu@softwire.com>
2024-07-03 14:09:22 +0300

    add knight moves

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,16 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const moveArray: any[] = new Array(0);
+
+        const dx = [-2, -1, 1, 2, 2, 1, -1, -2];
+        const dy = [-1, -2, -2, -1, 1, 2, 2, 1];
+
+        for (let index = 0; index < dx.length; index++) {
+            moveArray.push(new Square(currentSquare.row + dx[index], currentSquare.col + dy[index]));
+        }
+
+        return moveArray;
     }
 }
