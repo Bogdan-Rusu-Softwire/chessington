e3d431b7853243e2c7ee202ac9c14de5a222d47d
Bogdan Rusu <Bogdan.Rusu@softwire.com>
2024-07-03 15:27:24 +0300

    add queen takeover logic

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -13,48 +13,80 @@ export default class Queen extends Piece {
         const moveArray: any[] = new Array(0);
 
         for (let row = currentSquare.row + 1, col = currentSquare.col + 1 ; row < 8 && col < 8; row++, col++) {
+            if (board.isEnemyPiece(currentSquare, new Square(row, col))) {
+                moveArray.push(new Square(row, col));
+                break;
+            }
             if (!board.isReachable(new Square(row, col)))
                 break;
             moveArray.push(new Square(row, col));
         }
 
         for (let row = currentSquare.row - 1, col = currentSquare.col + 1 ; row >= 0 && col < 8; row--, col++) {
+            if (board.isEnemyPiece(currentSquare, new Square(row, col))) {
+                moveArray.push(new Square(row, col));
+                break;
+            }
             if (!board.isReachable(new Square(row, col)))
                 break;
             moveArray.push(new Square(row, col));
         }
 
         for (let row = currentSquare.row + 1, col = currentSquare.col - 1 ; row < 8 && col >= 0; row++, col--) {
+            if (board.isEnemyPiece(currentSquare, new Square(row, col))) {
+                moveArray.push(new Square(row, col));
+                break;
+            }
             if (!board.isReachable(new Square(row, col)))
                 break;
             moveArray.push(new Square(row, col));
         }
 
         for (let row = currentSquare.row - 1, col = currentSquare.col - 1 ; row >= 0 && col >= 0; row--, col--) {
+            if (board.isEnemyPiece(currentSquare, new Square(row, col))) {
+                moveArray.push(new Square(row, col));
+                break;
+            }
             if (!board.isReachable(new Square(row, col)))
                 break;
             moveArray.push(new Square(row, col));
         }
 
         for (let row= currentSquare.row + 1; row < 8; row ++) {
+            if (board.isEnemyPiece(currentSquare, new Square(row, currentSquare.col))) {
+                moveArray.push(new Square(row, currentSquare.col));
+                break;
+            }
             if (!board.isReachable(new Square(row, currentSquare.col)))
                 break;
             moveArray.push(new Square(row, currentSquare.col));
         }
 
         for (let row= currentSquare.row - 1; row >= 0; row --) {
+            if (board.isEnemyPiece(currentSquare, new Square(row, currentSquare.col))) {
+                moveArray.push(new Square(row, currentSquare.col));
+                break;
+            }
             if (!board.isReachable(new Square(row, currentSquare.col)))
                 break;
             moveArray.push(new Square(row, currentSquare.col));
         }
 
         for (let col = currentSquare.col + 1; col < 8; col ++) {
+            if (board.isEnemyPiece(currentSquare, new Square(currentSquare.row, col))) {
+                moveArray.push( new Square(currentSquare.row, col));
+                break;
+            }
             if (!board.isReachable(new Square(currentSquare.row, col)))
                 break;
             moveArray.push(new Square(currentSquare.row, col));
         }
 
         for (let col = currentSquare.col - 1; col >= 0; col --) {
+            if (board.isEnemyPiece(currentSquare, new Square(currentSquare.row, col))) {
+                moveArray.push( new Square(currentSquare.row, col));
+                break;
+            }
             if (!board.isReachable(new Square(currentSquare.row, col)))
                 break;
             moveArray.push(new Square(currentSquare.row, col));
