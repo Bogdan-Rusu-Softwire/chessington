7df6d97dba84cbf791022e077db69c50a0b4e3b2
Bogdan Rusu <Bogdan.Rusu@softwire.com>
2024-07-03 14:48:44 +0300

    add queen collision check

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -13,30 +13,50 @@ export default class Queen extends Piece {
         const moveArray: any[] = new Array(0);
 
         for (let row = currentSquare.row + 1, col = currentSquare.col + 1 ; row < 8 && col < 8; row++, col++) {
+            if (!board.isReachable(new Square(row, col)))
+                break;
             moveArray.push(new Square(row, col));
         }
 
         for (let row = currentSquare.row - 1, col = currentSquare.col + 1 ; row >= 0 && col < 8; row--, col++) {
+            if (!board.isReachable(new Square(row, col)))
+                break;
             moveArray.push(new Square(row, col));
         }
 
         for (let row = currentSquare.row + 1, col = currentSquare.col - 1 ; row < 8 && col >= 0; row++, col--) {
+            if (!board.isReachable(new Square(row, col)))
+                break;
             moveArray.push(new Square(row, col));
         }
 
         for (let row = currentSquare.row - 1, col = currentSquare.col - 1 ; row >= 0 && col >= 0; row--, col--) {
+            if (!board.isReachable(new Square(row, col)))
+                break;
             moveArray.push(new Square(row, col));
         }
 
-        for (let row = 0; row < 8; row ++) {
-            if (row == currentSquare.row)
-                continue;
+        for (let row= currentSquare.row + 1; row < 8; row ++) {
+            if (!board.isReachable(new Square(row, currentSquare.col)))
+                break;
             moveArray.push(new Square(row, currentSquare.col));
         }
 
-        for (let col = 0; col < 8; col ++) {
-            if (col == currentSquare.col)
-                continue;
+        for (let row= currentSquare.row - 1; row >= 0; row --) {
+            if (!board.isReachable(new Square(row, currentSquare.col)))
+                break;
+            moveArray.push(new Square(row, currentSquare.col));
+        }
+
+        for (let col = currentSquare.col + 1; col < 8; col ++) {
+            if (!board.isReachable(new Square(currentSquare.row, col)))
+                break;
+            moveArray.push(new Square(currentSquare.row, col));
+        }
+
+        for (let col = currentSquare.col - 1; col >= 0; col --) {
+            if (!board.isReachable(new Square(currentSquare.row, col)))
+                break;
             moveArray.push(new Square(currentSquare.row, col));
         }
 
