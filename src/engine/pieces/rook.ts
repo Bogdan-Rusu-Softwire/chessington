228d81c01f2de2807827b7994c73a03b5202cede
Bogdan Rusu <Bogdan.Rusu@softwire.com>
2024-07-03 13:26:56 +0300

    add rook basic moves

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,21 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const moveArray: any[] = new Array(0);
+
+        for (let row = 0; row < 8; row ++) {
+            if (row == currentSquare.row)
+                continue;
+            moveArray.push(new Square(row, currentSquare.col));
+        }
+
+        for (let col = 0; col < 8; col ++) {
+            if (col == currentSquare.col)
+                continue;
+            moveArray.push(new Square(currentSquare.row, col));
+        }
+
+        return moveArray;
     }
 }
