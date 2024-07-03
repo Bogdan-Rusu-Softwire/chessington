e3db4a38cf681b37c57ac6b09a3bac2c6045a10e
Bogdan Rusu <Bogdan.Rusu@softwire.com>
2024-07-03 14:44:21 +0300

    add bishop collision check

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -12,18 +12,26 @@ export default class Bishop extends Piece {
         const currentSquare = board.findPiece(this);
         const moveArray: any[] = new Array(0);
         for (let row = currentSquare.row + 1, col = currentSquare.col + 1 ; row < 8 && col < 8; row++, col++) {
+            if (!board.isReachable(new Square(row, col)))
+                break;
             moveArray.push(new Square(row, col));
         }
 
         for (let row = currentSquare.row - 1, col = currentSquare.col + 1 ; row >= 0 && col < 8; row--, col++) {
+            if (!board.isReachable(new Square(row, col)))
+                break;
             moveArray.push(new Square(row, col));
         }
 
         for (let row = currentSquare.row + 1, col = currentSquare.col - 1 ; row < 8 && col >= 0; row++, col--) {
+            if (!board.isReachable(new Square(row, col)))
+                break;
             moveArray.push(new Square(row, col));
         }
 
         for (let row = currentSquare.row - 1, col = currentSquare.col - 1 ; row >= 0 && col >= 0; row--, col--) {
+            if (!board.isReachable(new Square(row, col)))
+                break;
             moveArray.push(new Square(row, col));
         }
 
