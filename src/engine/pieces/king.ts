702593eedcd49aad0a753a556c834961b7bebb40
Bogdan Rusu <Bogdan.Rusu@softwire.com>
2024-07-03 14:14:20 +0300

    add king moves

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,15 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const moveArray = new Array(0);
+        const dx = [-1, -1, -1, 0, 0, 1, 1, 1];
+        const dy = [-1, 0, 1, -1, 1, -1, 0, 1];
+
+        for (let index = 0; index < 8; index++) {
+            moveArray.push(new Square(currentSquare.row + dx[index], currentSquare.col + dy[index]));
+        }
+
+        return moveArray;
     }
 }
